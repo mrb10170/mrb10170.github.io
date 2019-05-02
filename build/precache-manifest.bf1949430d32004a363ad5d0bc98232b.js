5fbaef2fd220a4a90d1b7dbbcd5876f0b248cd68
mrb10170 <50208581+mrb10170@users.noreply.github.com>
2019-05-02 16:36:51 -0400

    build folder

---
+++ build/precache-manifest.bf1949430d32004a363ad5d0bc98232b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef40b93b05c846e9130817de0b64720c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c18e48bfeed085253aa2",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c18e48bfeed085253aa2",
+    "url": "/static/js/2.91e3e486.chunk.js"
+  },
+  {
+    "revision": "6faef894732fe782cc15",
+    "url": "/static/js/main.0c27612a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
