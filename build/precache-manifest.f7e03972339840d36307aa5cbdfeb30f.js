928218f8a22369747351f7bc997abe8211bd6c83
mrb10170 <50208581+mrb10170@users.noreply.github.com>
2019-05-02 16:52:23 -0400

    fixing project name

---
+++ build/precache-manifest.f7e03972339840d36307aa5cbdfeb30f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a142e4f548c08e7785b0efa77f32a5ab",
+    "url": "/mrb10170.github.io/index.html"
+  },
+  {
+    "revision": "e28824a475649b2639b1",
+    "url": "/mrb10170.github.io/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e28824a475649b2639b1",
+    "url": "/mrb10170.github.io/static/js/2.9960a0ca.chunk.js"
+  },
+  {
+    "revision": "a796287b7b73792455c3",
+    "url": "/mrb10170.github.io/static/js/main.ab63f4f2.chunk.js"
+  },
+  {
+    "revision": "a79e448210521f56da40",
+    "url": "/mrb10170.github.io/static/js/runtime~main.ab803c7d.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/mrb10170.github.io/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myapp/precache-manifest.3fbef78001521b8221563759cf4d64ac.js"
+  "/mrb10170.github.io/precache-manifest.f7e03972339840d36307aa5cbdfeb30f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mrb10170.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
